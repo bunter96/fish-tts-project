8c9f9e4338d388df597c55b55d9386cf1bab5e04
bunter96 <jazzcashgiveaway@gmail.com>
2025-03-16 02:28:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ cred = credentials.Certificate(cred_data)
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://my-tts-web-app-test.web.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
